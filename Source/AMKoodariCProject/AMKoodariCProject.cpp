42a530a5fd5ed7da3770e19c1fb0498f1fdc10f9
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2020-03-24 16:40:44 +0200

    File upload.

---
+++ Source/AMKoodariCProject/AMKoodariCProject.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "AMKoodariCProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AMKoodariCProject, "AMKoodariCProject" );
+ 
